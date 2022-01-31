97f4a1bea5db0da813bb4b77c5ebf629913e628e
Muhammad Abdullah Khan <abdullahkhan.smiu@gmail.com>
2022-02-01 02:10:22 +0500

    Change Made

--- host/settings.py
+++ host/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-atn!p81=h92g+tom0l&7#&-_#td*h=7-4q38a81&v-(@^bth0!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
